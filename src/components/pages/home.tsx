eec321d6110c4a47324a2d8f92fdd467d354bdf4
toan5ks1 <vipteamvnclass@gmail.com>
2024-04-12 13:03:34 +0700

    setup route

---
+++ src/components/pages/home.tsx
@@ -0,0 +1,12 @@
+import { MainNav } from '../layout/main-nav';
+
+export const Home = () => {
+  return (
+    <div className="text-center">
+      <MainNav />
+      <h1 className="text-4xl font-bold">
+        electron-react-boilerplate with tailwind
+      </h1>
+    </div>
+  );
+};
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,27 +1,14 @@
 import { Route, MemoryRouter as Router, Routes } from 'react-router-dom';
-import icon from '../../assets/icon.png';
-import { MainNav } from '../components/layout/main-nav';
 import './App.css';
 import AppProvider from './providers/app';
-
-const Hello = () => {
-  return (
-    <div className="text-center">
-      <img src={icon} alt="logo" className="h-48 w-48 mx-auto" />
-      <h1 className="text-8xl font-bold">
-        electron-react-boilerplate with tailwind
-      </h1>
-    </div>
-  );
-};
+import { Home } from '../components/pages/home';
 
 export default function App() {
   return (
     <AppProvider>
-      <MainNav />
       <Router>
         <Routes>
-          <Route path="/" element={<Hello />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </AppProvider>
