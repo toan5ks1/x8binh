30d826818e529b1c7f1363192fe615059cfbb463
dotuankietx <dotuannkiet2604@gmail.com>
2024-04-14 16:57:21 +0700

    Fix: UI terminal

--- src/games/rik/pages/ongame.tsx
+++ src/games/rik/pages/ongame.tsx
@@ -1,10 +1,11 @@
-import { DoubleArrowRightIcon, TrashIcon } from '@radix-ui/react-icons';
+import { PaperPlaneIcon, TrashIcon } from '@radix-ui/react-icons';
 import React, { useEffect, useState } from 'react';
 import { MainNav } from '../components/layout/main-nav';
 
 export const OnGame: React.FC = () => {
   const [data, setData] = useState<unknown[]>([]);
   const [command, setCommand] = useState('');
+  const [roomId, setRoomId] = useState('');
 
   const parseData = (dataString: string) => {
     try {
@@ -76,32 +77,74 @@ export const OnGame: React.FC = () => {
     }
   };
 
+  const openPuppeteer = (): void => {
+    window.electron.ipcRenderer.openPuppeteer();
+  };
+
+  const clearData = () => {
+    setData([]);
+  };
+
+  function joinRoom(roomId: string): any {
+    window.electron.ipcRenderer.executeScript('execute-script', [
+      `__require('GamePlayManager').default.getInstance().joinRoom(${roomId},0,'',true);`,
+    ]);
+  }
+
   return (
     <div className="text-center h-full">
       <MainNav />
       <div className="flex flex-col">
-        <div className="w-full overflow-y-scroll h-[500px] max-h-[500px]">
-          <div className="flex flex-col justify-between terminal relative">
-            <div className=" absolute right-[5px] flex flex-row gap-[10px]">
-              <div className=" border-white bg-[#1e1e1e] rounded-[5px] border-[2px] p-[5px] w-[30px] h-[30px] flex justify-center items-center">
-                <button>
-                  <TrashIcon />
-                </button>
+        <div className="w-full overflow-y-scroll ">
+          <div className="flex flex-col terminal relative">
+            <div className="flex flex-row justify-end bg-[#1e1e1e] gap-[10px]">
+              <div
+                onClick={clearData}
+                className=" border-white bg-[#1e1e1e]  hover:bg-slate-400 rounded-[5px] border-[2px] py-[5px] px-[10px] flex justify-center items-center cursor-pointer"
+              >
+                <TrashIcon />
               </div>
 
               <div
                 style={{ fontFamily: 'monospace' }}
-                className="border-white rounded-[5px] px-[5px] flex items-center bg-[#1e1e1e]  border-[2px] hover:bg-slate-400"
+                className="border-white rounded-[5px] px-[5px] flex items-center bg-[#1e1e1e]   border-[2px] hover:bg-slate-400 cursor-pointer"
               >
                 <button onClick={createRoom}>Create Room</button>
               </div>
+              <div
+                style={{ fontFamily: 'monospace' }}
+                className="rounded-[5px] flex items-center bg-[#1e1e1e]  cursor-pointer border-[2px]  "
+              >
+                <input
+                  type="text"
+                  className="command-input "
+                  style={{ fontFamily: 'monospace' }}
+                  placeholder="Type messsage..."
+                  onChange={(e) => setRoomId(e.target.value)}
+                />
+                <div
+                  style={{ fontFamily: 'monospace' }}
+                  className="rounded-[5px] px-[5px] flex items-center bg-[#1e1e1e] cursor-pointer hover:bg-slate-400"
+                >
+                  <button onClick={joinRoom(roomId)}>Join Room</button>
+                </div>
+              </div>
+              <div
+                style={{ fontFamily: 'monospace' }}
+                className="border-white rounded-[5px] px-[5px] flex items-center bg-[#1e1e1e]  border-[2px] hover:bg-slate-400 cursor-pointer"
+              >
+                <button onClick={openPuppeteer}>Open Pupperteer</button>
+              </div>
             </div>
 
-            <div>
+            <div
+              id="messageContainer"
+              className="flex flex-col grow h-full mb-[30px] overflow-y-scroll"
+            >
               {data.map((item, index) => (
                 <div
                   key={index}
-                  className="flex justify-start text-left font-bold"
+                  className="flex justify-start text-left font-bold command-input"
                   style={{ fontFamily: 'monospace' }}
                   dangerouslySetInnerHTML={{
                     __html: highlightSyntax(JSON.stringify(item, null, 2)),
@@ -110,10 +153,10 @@ export const OnGame: React.FC = () => {
               ))}
             </div>
             <div className="sticky bottom-0">
-              <div className="flex flex-row justify-between border-white border-[2px] p-[5px]">
+              <div className="flex flex-row justify-between border-white border-[2px] p-[5px] bg-[#1e1e1e]">
                 <input
                   type="text"
-                  className="command-input"
+                  className="bg-[#1e1e1e] w-full"
                   style={{ fontFamily: 'monospace' }}
                   placeholder="Type messsage..."
                   onChange={(e) => setCommand(e.target.value)}
@@ -124,9 +167,9 @@ export const OnGame: React.FC = () => {
                 <div
                   style={{ fontFamily: 'monospace' }}
                   onClick={sendMessage}
-                  className="border-white rounded-[5px] px-[5px] flex justify-center gap-[3px] items-center border-[2px] hover:bg-slate-400"
+                  className="border-white  rounded-[5px] px-[5px] flex justify-center gap-[3px] items-center border-[2px] hover:bg-slate-400"
                 >
-                  <DoubleArrowRightIcon />
+                  <PaperPlaneIcon />
                   <p>Send</p>
                 </div>
               </div>
